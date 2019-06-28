2d3fb4b774ee2b5a92f0e3dce960e1d86c48ca37
Administrator <Administrator@DKUY20VEX0T5VDC>
2019-06-28 15:31:03 +0800

    就看美女看不好

---
+++ src/cn/bdqn/text/Usermapper.java
@@ -0,0 +1,11 @@
+package cn.bdqn.text;
+
+public class Usermapper {
+
+	public static void main(String[] args) {
+		System.out.println("你好");
+		System.out.println("你好");
+		System.out.println("我是你的爹地");
+	}
+
+}
